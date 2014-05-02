f517083bf2c5b60de16207fb75875dd06d33ab36
netyum <netyum@163.com>
2014-05-02 22:14:39 +0800

    add Yii.php

---
+++ Yii.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Yii bootstrap file.
+ *
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+/**
+ * Gets the application start timestamp.
+ */
+defined('YII_BEGIN_TIME') or define('YII_BEGIN_TIME', microtime(true));
+/**
+ * This constant defines the framework installation directory.
+ */
+defined('YII_PATH') or define('YII_PATH', __DIR__);
+/**
+ * This constant defines whether the application should be in debug mode or not. Defaults to false.
+ */
+defined('YII_DEBUG') or define('YII_DEBUG', false);
+/**
+ * This constant defines in which environment the application is running. Defaults to 'prod', meaning production environment.
+ * You may define this constant in the bootstrap script. The value could be 'prod' (production), 'dev' (development), 'test', 'staging', etc.
+ */
+defined('YII_ENV') or define('YII_ENV', 'prod');
+/**
+ * Whether the the application is running in production environment
+ */
+defined('YII_ENV_PROD') or define('YII_ENV_PROD', YII_ENV === 'prod');
+/**
+ * Whether the the application is running in development environment
+ */
+defined('YII_ENV_DEV') or define('YII_ENV_DEV', YII_ENV === 'dev');
+/**
+ * Whether the the application is running in testing environment
+ */
+defined('YII_ENV_TEST') or define('YII_ENV_TEST', YII_ENV === 'test');
+
+/**
+ * This constant defines whether error handling should be enabled. Defaults to true.
+ */
+defined('YII_ENABLE_ERROR_HANDLER') or define('YII_ENABLE_ERROR_HANDLER', true);
+/**
+ * Yii is a helper class serving common framework functionalities.
+ *
+ * It extends from [[\yii\BaseYii]] which provides the actual implementation.
+ * By writing your own Yii class, you can customize some functionalities of [[\yii\BaseYii]].
+ *
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ */
+use yii\BaseYii;
+
+class Yii extends \yii\BaseYii
+{
+
+}
+Yii::setAlias("@yii", __DIR__);
+spl_autoload_register(['Yii', 'autoload'], true, true);
+Yii::$classMap = include(__DIR__ . '/classes.php');
+Yii::$container = new yii\di\Container;
